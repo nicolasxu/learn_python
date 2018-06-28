17473caeb9b3e239b6beeeb40bf2c7b3daecd07d
nick xu <nicolas.xu@berkshiregrey.com>
2018-06-28 15:44:12 -0400

    update

---
+++ 23. ternary operator.py
@@ -0,0 +1,7 @@
+23. ternary operator
+
+# https://stackoverflow.com/questions/2802726/putting-a-simple-if-then-else-statement-on-one-line
+
+value_when_true if condition else value_when_false
+
+res = 'Yes' if fruit == 'Apple' else 'No'
