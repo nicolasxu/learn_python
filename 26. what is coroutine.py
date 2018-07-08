a39f4974c1ae1cf30718b48f05ee45d4752991cf
Polar Bottle <shenxinxu@zz11.fios-router.home>
2018-07-08 12:17:21 -0400

    update

---
+++ 26. what is coroutine.py
@@ -0,0 +1,16 @@
+
+str = 'Coroutine is implemented as an extension to generators '
+'''
+
+
+Python can work around all these issues with coroutines. 
+
+Coroutines let you have many seemingly simultaneous 
+functions in your Python programs. They’re implemented as 
+an extension to generators 
+
+
+The cost of starting a generator coroutine is a function call. 
+Once active, they each use less than 1 KB of memory until they’re exhausted.
+
+'''
